be0346c8c42219e903c4110a9265a9f4ffcdf5a8
Bartosz Gembala <bartoszgembala95@gmail.com>
2022-04-14 23:53:47 +0200

    added PORT

--- app.js
+++ app.js
@@ -52,8 +52,8 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.v1tgq.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(
-    app.listen(5000, () => {
-      console.log("Listening at port 5000");
+    app.listen(process.env.PORT || 5000, () => {
+      console.log("Listening");
     })
   )
   .catch((err) => {
