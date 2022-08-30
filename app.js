a4fdad32168214633d10da74d35c9ff58fd77f6b
BartoszGembala <92273516+FaudSkury@users.noreply.github.com>
2022-08-30 23:02:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.v1tgq.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+   
   )
   .then(
     app.listen(process.env.PORT || 5000, () => {
